7bec4c9e968deab761c263eac0f39970b4991ec5
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-18 18:29:59 -0300

    Create themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react'
-import { createTheme, CssBaseline } from '@mui/material'
+import { CssBaseline } from '@mui/material';
+
+import { darkTheme, lightTheme } from '@/themes'
 
 import '@/styles/globals.css'
 
-const basicTheme = createTheme({
-  palette: {
-    mode: 'dark'
-  }
-})
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={basicTheme}>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
