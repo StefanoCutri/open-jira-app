0d3a81afab59229fee19a693340a857bdee065b2
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-20 20:21:26 -0300

    Change cards content

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,28 @@ export default function HomePage() {
   return (
     <Layout title="Home - OpenJira">
       <Grid container spacing={2}>
+
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="Pending" />
-            <CardContent>
               <EntryList status="pending" />
-            </CardContent>
           </Card>
         </Grid>
+
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="In Progress" />
-            <CardContent>
               <EntryList status="in-progress" />
-            </CardContent>
           </Card>
         </Grid>
+
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="Completed" />
-            <CardContent>
               <EntryList status="finished" />
-            </CardContent>
           </Card>
         </Grid>
+
       </Grid>
     </Layout>
   );
