6dd277d2deff46695e08afca6d9ccb52e5f4a115
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-20 20:40:49 -0300

    NewEntry component

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+
+export const NewEntry = () => {
+  return (
+    <Box sx={{ marginBottom: 2, paddingX: 2 }}>
+      <Button startIcon={<AddIcon />} fullWidth variant="outlined"></Button>
+      
+      <TextField
+        fullWidth
+        sx={{ marginTop: 2, marginBottom: 1 }}
+        placeholder="New entry"
+        autoFocus
+        multiline
+        label="New entry"
+        helperText="Enter new value"
+      />
+
+      <Box display="flex" justifyContent="space-between">
+        <Button variant="text">Cancel</Button>
+        <Button
+          endIcon={<SaveOutlinedIcon />}
+          color="secondary"
+          variant="outlined"
+        ></Button>
+      </Box>
+    </Box>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,32 @@
-import { Card, CardContent, CardHeader, Grid } from "@mui/material";
+import { Card, CardHeader, Grid } from "@mui/material";
 import { Layout } from "@/components/layout";
-import { EntryList } from "@/components/ui";
+import { EntryList, NewEntry } from "@/components/ui";
 
 export default function HomePage() {
   return (
     <Layout title="Home - OpenJira">
       <Grid container spacing={2}>
-
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="Pending" />
-              <EntryList status="pending" />
+            <NewEntry />
+            <EntryList status="pending" />
           </Card>
         </Grid>
 
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="In Progress" />
-              <EntryList status="in-progress" />
+            <EntryList status="in-progress" />
           </Card>
         </Grid>
 
         <Grid item xs={2} sm={4}>
           <Card sx={{ height: "calc(100vh - 100px)" }}>
             <CardHeader title="Completed" />
-              <EntryList status="finished" />
+            <EntryList status="finished" />
           </Card>
         </Grid>
-
       </Grid>
     </Layout>
   );
