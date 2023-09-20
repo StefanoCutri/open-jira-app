15480c565faac1fc3d2682be490ea6664024a4a6
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-20 20:48:27 -0300

    Show EntryCard content conditionally

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 2 }}>
-      <Button startIcon={<AddIcon />} fullWidth variant="outlined"></Button>
-      
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder="New entry"
-        autoFocus
-        multiline
-        label="New entry"
-        helperText="Enter new value"
-      />
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder="New entry"
+            autoFocus
+            multiline
+            label="New entry"
+            helperText="Enter new value"
+          />
 
-      <Box display="flex" justifyContent="space-between">
-        <Button variant="text">Cancel</Button>
-        <Button
-          endIcon={<SaveOutlinedIcon />}
-          color="secondary"
-          variant="outlined"
-        ></Button>
-      </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Button variant="text" onClick={() => setIsAdding(false)}>Cancel</Button>
+            <Button
+              endIcon={<SaveOutlinedIcon />}
+              color="secondary"
+              variant="outlined"
+            >Save</Button>
+          </Box>
+        </>
+      ) : (
+        <Button startIcon={<AddIcon />} fullWidth variant="outlined" onClick={() => setIsAdding(true)}>Add</Button>
+      )}
     </Box>
   );
 };
