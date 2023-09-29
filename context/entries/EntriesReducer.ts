48e5c01ef35da17f0de48f3075cf4fa6b7ba3630
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-29 20:19:32 -0300

    EntriesReducer action to refresh actions

--- context/entries/EntriesReducer.ts
+++ context/entries/EntriesReducer.ts
@@ -3,7 +3,8 @@ import { EntriesState } from ".";
 
 type EntriesActionType =
   | { type: "[Entry] Add-Entry"; payload: Entry }
-  | { type: "[Entry] Update-Entry"; payload: Entry };
+  | { type: "[Entry] Update-Entry"; payload: Entry }
+  | { type: "[Entry] Refresh-Entries"; payload: Entry[] };
 
 export const entriesReducer = (
   state: EntriesState,
@@ -28,6 +29,12 @@ export const entriesReducer = (
           return entry;
         }),
       };
+
+    case "[Entry] Refresh-Entries":
+      return {
+        ...state,
+        entries: [...action.payload]
+      }
 
     default:
       return state;
