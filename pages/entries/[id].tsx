5fe4995425d2ae98e210bb9d10f96ad7a0e8d57e
Stefano Cutri <stefanocutristecco@gmail.com>
2023-10-09 20:07:07 -0300

    Entry Page style

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import {
+  capitalize,
   Button,
   Card,
   CardActions,
   CardContent,
   CardHeader,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
   Grid,
+  Radio,
+  RadioGroup,
   TextField,
+  IconButton,
 } from "@mui/material";
 import SaveAltOutlined from "@mui/icons-material/SaveAltOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { Layout } from "@/components/layout";
+import { EntryStatus } from "@/interfaces";
+
+const validStatus: EntryStatus[] = ["pending", "in-progress", "finished"];
 
 const EntryPage = () => {
   return (
@@ -27,6 +38,20 @@ const EntryPage = () => {
                 multiline
                 label="New Entry"
               />
+              <FormControl>
+                <FormLabel>Status:</FormLabel>
+
+                <RadioGroup row>
+                  {validStatus.map((option) => (
+                    <FormControlLabel
+                      key={option}
+                      value={option}
+                      control={<Radio />}
+                      label={capitalize(option)}
+                    ></FormControlLabel>
+                  ))}
+                </RadioGroup>
+              </FormControl>
             </CardContent>
             <CardActions>
               <Button
@@ -40,6 +65,14 @@ const EntryPage = () => {
           </Card>
         </Grid>
       </Grid>
+      <IconButton sx={{
+        position: 'fixed',
+        bottom: 30,
+        right: 30,
+        backgroundColor: 'error.dark'
+      }}>
+        <DeleteOutlinedIcon />
+      </IconButton>
     </Layout>
   );
 };
